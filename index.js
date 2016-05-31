7e78264e14664009e40782799eeafc740191b95c
punit1108 <punitkpal@gmail.com>
2016-05-31 13:57:39 +0530

    iAmAnIdiot

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 'use strict';
 
 var facebook = require("./lib/services/facebook.service.js");
-var facebook = require("./lib/services/github.service.js");
+var github = require("./lib/services/github.service.js");
 
 module.exports = {
     facebook: facebook,
     github: github
 };
-
-// facebook(function(res) {
-//         console.log(res);
-// });
